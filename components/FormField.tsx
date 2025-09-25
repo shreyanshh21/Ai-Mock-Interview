68575491b92a40cc3a0651ed1c1cb7e1d74174bf
Shreyansh Arpit <shreyanshpandey083@gmail.com>
2025-09-25 12:07:23 +0530

    setup app , file and folder structure, routing and auth pages

---
+++ components/FormField.tsx
@@ -0,0 +1,41 @@
+import React from "react"
+import { Controller, Control, FieldValues, Path } from "react-hook-form"
+import { FormItem, FormLabel, FormControl, FormDescription, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+interface FormFieldProps<T extends FieldValues> {
+  control: Control<T>
+  name: Path<T>
+  label: string
+  placeholder?: string
+  type?: "text" | "email" | "password" | "file"
+  description?: string
+}
+
+const FormField = <T extends FieldValues>({
+  control,
+  name,
+  label,
+  placeholder,
+  type = "text",
+  description,
+}: FormFieldProps<T>) => {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className="label">{label}</FormLabel>
+          <FormControl>
+            <Input className="input" type={type} placeholder={placeholder} {...field} />
+          </FormControl>
+          {description && <FormDescription>{description}</FormDescription>}
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
+
+export default FormField
